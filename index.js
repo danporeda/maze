8b07282a1d21c69313e78181b4e0bc4ebb3bb71c
Dan <danporeda@yahoo.com>
2023-12-28 01:14:37 -0500

    completed stepthroughcell function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
+const unitLength = width / cells;
+const unitHeight = height / cells;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -28,9 +31,84 @@ World.add(world, walls);
 
 // Maze Generation
 
-const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const shuffle = (arr) => {
+  let counter = arr.length;
 
-console.log(grid);
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+}
+
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  if (grid[row][column]) {
+    return;
+  }
+
+  grid[row][column] = true;
+  
+  const neighbors = shuffle([
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left']
+  ]);
+  
+  for (let neighbor of neighbors) {
+    console.log(`row: ${row} column: ${column}`);
+    const [nextRow, nextColumn, direction] = neighbor;
+    
+    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+      console.log('< > continue', direction);
+      continue;
+    }
+
+    if (grid[nextRow][nextColumn]) {
+      console.log('cell true continue', direction);
+      continue;
+    }
+
+    if (direction === 'left') {
+      verticals[row][column - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][column] = true;
+    } else if (direction === 'up') {
+      horizontals[row - 1][column] = true;
+    } else if (direction === 'down') {
+      horizontals[row][column] = true;
+    }
+    console.log(nextRow, nextColumn);
+    stepThroughCell(nextRow, nextColumn);
+  }
+};
+console.log(`startRow: ${startRow}, startColumn: ${startColumn}`);
+stepThroughCell(startRow, startColumn);
+
+horizontals.forEach(row => {
+  row.forEach((open) => {
+    if (open){
+      return;
+    }
+
+
+  })
+})
 
 // Random Shapes
 // for (let i = 0; i < 20; i++) {
